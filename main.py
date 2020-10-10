f1de8e6302c0445e6dd7366152b7f3c428142efc
Yarvillian <lkc55555@aol.com>
2020-10-10 13:05:22 -0400

    commit

--- main.py
+++ main.py
@@ -16,12 +16,28 @@ NEWSAPI_TOKEN = os.getenv("NEWSAPI_TOKEN")
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 bot = discord.Client()
 
-#test for /top-headlines endpoint
-url = ('http://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=NEWSAPI_TOKEN')
-response = requests.get(url)
-print response.json()
+#test for /top-headlines endpoint STILL IN TESTING
+
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+    sources='bbc-news,the-verge',
+    category='business',
+    language='en',
+    country='us')
+
+# /v2/everything
+all_articles = newsapi.get_everything(q='bitcoin',
+    sources='bbc-news,the-verge',
+    domains='bbc.co.uk,techcrunch.com',
+    from_param='2017-12-01',
+    to='2017-12-12',
+    language='en',
+    sort_by='relevancy',
+    page=2)
+
+# /v2/sources
+sources = newsapi.get_sources()
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
 async def on_ready():
